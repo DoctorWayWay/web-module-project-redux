0cb1cc06de570b6982840ebb3e4bdb48e660edbb
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-02 14:40:57 -0700

    Combined the favoriteMovies and movie reducers in the index.js file in the reducers directory.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import movieReducer from './movieReducer';
+import favoritesReducer from "./favoriteMoviesReducer";
+import movieReducer from "./movieReducer";
 
-export default movieReducer;
+export default combineReducers({
+  favorites: favoritesReducer,
+  movie: movieReducer,
+});
